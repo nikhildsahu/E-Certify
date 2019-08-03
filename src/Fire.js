6066ad725fc5d9a14669b680e0ffdd24f3fc0224
Nikhil Sahu <45421987+nikhildsahu@users.noreply.github.com>
2019-08-03 15:49:29 +0530

    Update Fire.js

--- src/Fire.js
+++ src/Fire.js
@@ -1,11 +1,11 @@
 import firebase from "firebase";
 var config = {
-  apiKey: "AIzaSyCRs0CDvdhpnDmsvumVloYwDlFfBctAQWs",
-  authDomain: "ecertify.firebaseapp.com",
-  databaseURL: "https://ecertify.firebaseio.com",
-  projectId: "ecertify",
-  storageBucket: "ecertify.appspot.com",
-  messagingSenderId: "410122053441"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 var fire = firebase.initializeApp(config);
 export default fire;
